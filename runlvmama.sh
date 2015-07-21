fbdeb7a7b5fa2a20fc30126fe685fb5c12e53000
dacaiguoguo <dacaiguoguo@163.com>
2015-07-21 14:15:38 +0800

    add run lvmama.sh

---
+++ runlvmama.sh
@@ -0,0 +1,4 @@
+kill -9 `cat /tmp/django.pid`
+echo 'restart django....'
+
+python /tmp/lvmama/manage.py runfcgi maxchildren=8  maxspare=3 minspare=1 method=prefork  pidfile=/tmp/django.pid host=127.0.0.1 port=9001 outlog=/tmp/dj.out errlog=/tmp/dj.error
