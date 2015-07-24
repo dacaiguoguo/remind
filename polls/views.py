7bac6b58f7194e51b6eee8cdc008639fbb20b61e
dacaiguoguo <dacaiguoguo@163.com>
2015-07-24 14:48:27 +0800

    add all

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(requset):
+    return HttpResponse(u"欢迎光临 大菜果果")
