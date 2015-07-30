db77c4db2d4399f04535b354bf845938a74605d3
dacaiguoguo <dacaiguoguo@gmail.com>
2015-07-30 21:03:21 +0800

    home

--- lvmama/urls.py
+++ lvmama/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from blog.views import home
 urlpatterns = [
-    url(r'^$', home),
+    url(r'^$',home),
     url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
